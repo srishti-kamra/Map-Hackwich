bf4186a066d8df50ace7fa2dd28e910b7a487e29
Srishti Kamra <srishtikamra07@gmail.com>
2023-02-08 09:38:53 -0600

    Initial Commit

---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by Srishti Kamra  on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
